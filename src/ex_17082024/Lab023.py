1d410e7d2d4283e21683c9726af70dec3781fe26
Kartikey Sir <kartikey.333@gmail.com>
2024-08-31 13:29:55 +0530

    Commit

---
+++ src/ex_17082024/Lab023.py
@@ -0,0 +1,18 @@
+#X//Y
+#X%Y
+#X**Y
+#**
+
+print(5//2)  # Remainder=2
+
+print(5%2) # Quotient=1
+
+print(5**2) # Power=25
+
+# // and /
+# /--> div
+# //-> Quotient
+
+#Compare operator
+
+print(2 -- 3)
---
+++ src/ex_17082024/Lab024.py
@@ -0,0 +1,20 @@
+# Operaot
+
+# Not Operator
+
+is_kartikey_married=True
+print(not is_kartikey_married)
+
+# Is Operator
+
+# Logical Operator/Comparative Operator
+
+x=10
+y=20
+print(x>y)
+print(x<y)
+
+a=10
+b=20
+print(a>=b)
+print(a<=b)
